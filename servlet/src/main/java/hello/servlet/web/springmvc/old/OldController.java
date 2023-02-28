cb714d28f11163a773b6daf76e9063f880946bd4
lib26 <dladlsqja@naver.com>
2023-02-28 16:07:10 +0900

    feat: Spring MVC V1

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.mvc.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+// 스프링 빈의 이름을 URL 패턴으로 맞춘것
+// localhost:8080/springmvc/old-controller
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
