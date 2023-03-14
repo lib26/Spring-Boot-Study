a46db1a39b1e9ff83de1d165d8980bb324b06ff4
lib26 <dladlsqja@naver.com>
2023-03-14 12:44:54 +0900

    feat: 동적 html

---
+++ item-service/src/main/java/hello/itemservice/web/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.itemservice.web;
+
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // 자동으로 생성자 만들어서 의존성 주입
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items"; // 동적 html인 templates/basic/items 경로를 의미
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     * 스프링 빈으로 등록되고 의존성 주입 이후 실행
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+    }
+}
